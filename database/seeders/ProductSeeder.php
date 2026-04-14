7a99a20236fcc75cc8e164526ccb6803f7883af8
Luiz Eduardo <luiztsilva@protonmail.com>
2026-04-14 15:07:28 -0300

    corecao remocao bom

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 
 namespace Database\Seeders;
 
@@ -6,7 +6,6 @@ use App\Models\Product;
 use App\Models\ProductImage;
 use App\Models\Supplier;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 
 class ProductSeeder extends Seeder
@@ -43,7 +42,6 @@ class ProductSeeder extends Seeder
         ])->each(function ($product) {
 
             // 1. Criar o SEO
-            // meta_title e h1 são derivados do product->description (usado no frontend)
             $product->seo()->firstOrCreate(
                 [
                     'seoable_id' => $product->id,
@@ -53,7 +51,6 @@ class ProductSeeder extends Seeder
                     'meta_description' => "Compre agora " . $product->description . " com as melhores condições.",
                     'meta_keywords'    => str_replace(' ', ', ', $product->description),
                     'text1'            => "Descrição detalhada do produto " . $product->description,
-                    // text2, h2, etc., são nullables, então não precisam estar aqui.
                 ]
             );
 
@@ -66,7 +63,6 @@ class ProductSeeder extends Seeder
                     $imageContent = file_get_contents($imageUrl);
 
                     if ($imageContent) {
-                        // Baixa a imagem se não existir no storage
                         if (!Storage::disk('public')->exists('products/' . $imageName)) {
                             Storage::disk('public')->put('products/' . $imageName, $imageContent);
                         }
